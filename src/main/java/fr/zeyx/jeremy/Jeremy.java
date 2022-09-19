aea565edf06534ed338b781448c9a55a829d1746
ZeyXs <basile.gamer@gmail.com>
2022-09-19 10:43:15 +0200

    📚 | Fixed Licenses

--- src/main/java/fr/zeyx/jeremy/Jeremy.java
+++ src/main/java/fr/zeyx/jeremy/Jeremy.java
@@ -4,6 +4,7 @@ import fr.zeyx.jeremy.listeners.EventListener;
 import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
 
 @SuppressWarnings("ALL")
@@ -17,8 +18,8 @@ public class Jeremy {
         builder.setStatus(OnlineStatus.ONLINE);
         builder.setActivity(Activity.watching("ur mom"));
         builder.addEventListeners(new EventListener());
+        builder.enableIntents(GatewayIntent.GUILD_MESSAGES);
         builder.build();
-
     }
 
     public static void main(String[] args) {
