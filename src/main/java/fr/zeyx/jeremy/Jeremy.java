c8996fc441679fc77d9161f668fb3537c9eb9d4d
ZeyXs <basile.gamer@gmail.com>
2022-09-26 15:36:38 +0200

    📚 | Added getter for Dotenv & ShardManager

--- src/main/java/fr/zeyx/jeremy/Jeremy.java
+++ src/main/java/fr/zeyx/jeremy/Jeremy.java
@@ -3,23 +3,35 @@ package fr.zeyx.jeremy;
 import fr.zeyx.jeremy.listeners.EventListener;
 import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.OnlineStatus;
-import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
+import net.dv8tion.jda.api.sharding.ShardManager;
 
 @SuppressWarnings("ALL")
 public class Jeremy {
 
+    public final Dotenv config;
+    public final ShardManager shardManager;
+
     public Jeremy() {
-        Dotenv config = Dotenv.configure().load();
+        config = Dotenv.configure().load();
         String token = config.get("TOKEN");
 
         DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(token);
         builder.setStatus(OnlineStatus.ONLINE);
-        builder.setActivity(Activity.watching("ur mom"));
-        builder.addEventListeners(new EventListener());
-        builder.enableIntents(GatewayIntent.GUILD_MESSAGES);
-        builder.build();
+        builder.enableIntents(GatewayIntent.getIntents(GatewayIntent.ALL_INTENTS));
+
+        shardManager = builder.build();
+
+        shardManager.addEventListener(new EventListener());
+    }
+
+    public Dotenv getConfig() {
+        return config;
+    }
+
+    public ShardManager getShardManager() {
+        return shardManager;
     }
 
     public static void main(String[] args) {
